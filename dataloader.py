a2256ac91633dfe80bc00abdec036049e2385d30
Hasan Emir Akın <h.emirakn@gmail.com>
2023-04-09 22:14:23 +0300

    Implemented DataLoader class for issue #2

---
+++ dataloader.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+import torch
+
+class DataLoader:
+
+    def __init__(self, data):
+
+        """ A class to transform given data into torch tensors
+            Currently accepted data types are: 'pd.DataFrame' and 'np.ndarray' """
+        
+        self.accepted_formats = [pd.DataFrame,
+                                 np.ndarray]
+        
+        self.data_type = type(data)
+        
+        assert (self.data_type in self.accepted_formats), f"{self.data_type} is not an accepted data type"
+
+        if self.data_type == pd.DataFrame:
+
+            self.data = self.organize_df(data)
+        
+        else:
+            self.data = data
+    
+    def organize_df(self, df: pd.DataFrame):
+
+        """ Method for organizing a possbily unorganized dataframe while also making sure all entries are convertible to tensors
+            and keeping track of the operations done on the original dataframe"""
+
+        self.organized_df = df.copy()
+
+        self.operations = []
+
+        self.dates = None
+        self.data_columns = list(df.columns)
+
+        for ind, dtype in enumerate(df.dtypes):
+            col = df.dtypes.index[ind]
+
+            if dtype == object:
+                try:
+                    self.dates = pd.to_datetime(df[col])
+                    self.operations.append(f"Turned entries of column'{col}' into datetime")
+
+                    self.data_columns.remove(col)
+                    self.organized_df.pop(col)
+                    self.organized_df = self.organized_df.set_index(self.dates)
+                    self.organized_df.index.name = "Dates"
+                    self.operations.append("Set dates as an index")
+                    
+                except:
+                    
+                    try:
+                        float_vals = df[col].values.astype(np.float32)
+                        self.organized_df[col] = float_vals
+                        self.operations.append(f"Turned {col} entries into floats")
+                    except:
+                        raise Exception(f"Could not handle entries of column: '{col}' ")
+        
+        return self.organized_df.values
+
+    def to_tensor(self):
+
+        """Turn self.data into tensors"""
+
+        return torch.tensor(self.data)
+        
