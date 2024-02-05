a99610b07a08016adfa1d4058210826ff5360f29
hasanemirakin <h.emirakn@gmail.com>
2024-02-05 19:28:57 +0300

    Delete initialization methods for exponential smoothing as they are deprecated now

--- chronokit/exponential_smoothing/initialization/__init__.py
+++
@@ -1,8 +0,0 @@
-from .ets_methods import *  # noqa: F403
-from .initialization_methods import (
-    get_init_method,
-    get_smooth_method,
-    heuristic_initialization,
-    mle_initialization,
-)
-from .smoothing_methods import *  # noqa: F403
